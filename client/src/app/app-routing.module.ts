fff1ccf4c9e6ffe98dec82bd716393444e1c61fd
kashyap <kashyap.p@myntra.com>
2022-10-13 14:58:18 +0530

    tets commit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,24 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FacultyregistrationComponent } from './facultyregistration/facultyregistration.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { MeanMay22Component } from './mean-may22/mean-may22.component';
-import { SignupacknowledgeComponent } from './signupacknowledge/signupacknowledge.component';
 
-
-
-import { StudentsComponent } from './students/students.component';
-import { TrainerCoursesMeanComponent } from './trainer-courses-mean/trainer-courses-mean.component';
-
-import { TrainerHomeComponent } from './trainer-home/trainer-home.component';
-
-
-const routes: Routes = [{path:'',component:HomeComponent},{path:'login',component:LoginComponent},
-{path:'facultyregistration',component:FacultyregistrationComponent},{path: 'students', component:StudentsComponent} ,{path:'signupacknowledge',component:SignupacknowledgeComponent},
-{path:'forgotpassword',component:ForgotPasswordComponent},{path:'trainer-home',component:TrainerHomeComponent},{path:'trainer-mean',component:TrainerCoursesMeanComponent},
-{path:'mean-may22',component:MeanMay22Component}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,47 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { FacultyregistrationComponent } from './facultyregistration/facultyregistration.component';
-
-import { StudentsComponent } from './students/students.component';
-import { SignupacknowledgeComponent } from './signupacknowledge/signupacknowledge.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { TrainerHomeComponent } from './trainer-home/trainer-home.component';
-import { TrainerCoursesMeanComponent } from './trainer-courses-mean/trainer-courses-mean.component';
-
-import { MeanMay22Component } from './mean-may22/mean-may22.component';
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    LoginComponent,
-    FooterComponent,
-    HomeComponent,
- StudentsComponent,
- FacultyregistrationComponent,
- SignupacknowledgeComponent,
- ForgotPasswordComponent,
- TrainerHomeComponent,
- TrainerCoursesMeanComponent,
- MeanMay22Component,
-    StudentsComponent,
-     FacultyregistrationComponent
-],
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
-    FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
